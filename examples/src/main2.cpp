09d706a2971241dd349298346f99737234653889
Laurence <wanglaurence13@gmail.com>
2022-12-01 22:29:18 +0100

    operations & operators comparaison

---
+++ examples/src/main2.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include "ratio.hpp"
+
+int main(){
+
+    Ratio a(1,2);
+    std::cout << "a = "; 
+    a.displayRatio();
+
+    Ratio b(3,4);
+    std::cout << "b = ";
+    b.displayRatio();
+
+    Ratio c = a;
+
+    if(a == b){
+        std::cout << "error a != b" << std::endl;
+    }else{
+        std::cout << "success a != b"<<std::endl;
+    }
+
+
+    if( a == c){
+        std::cout <<"success a = c "<<std::endl;
+    }else{
+        std::cout << "error a = c "<<std::endl;
+    }
+
+
+    if(a != b){
+        std::cout << "success a != b" << std::endl;
+    }else{
+        std::cout << "error a != b"<<std::endl;
+    }
+
+
+    if( a != c){
+        std::cout <<"error a = c "<<std::endl;
+    }else{
+        std::cout << "success a = c "<<std::endl;
+    }
+
+
+    if(a > b){
+        std::cout << "error a < b" << std::endl;
+    }else{
+        std::cout << "success a < b"<<std::endl;
+    }
+
+    if(a < b){
+        std::cout << "sucess a < b" << std::endl;
+    }else{
+        std::cout << "error a < b"<<std::endl;
+    }
+
+    if(a >= b){
+        std::cout << "error a < b" << std::endl;
+    }else{
+        std::cout << "success a < b"<<std::endl;
+    }
+
+    if(a <= b){
+        std::cout << "success a < b" << std::endl;
+    }else{
+        std::cout << "error a < b"<<std::endl;
+    }
+
+
+    if(a >= c){
+        std::cout << "success a = c" << std::endl;
+    }else{
+        std::cout << "error a = c"<<std::endl;
+    }
+
+    if(a <= c){
+        std::cout << "success a = c" << std::endl;
+    }else{
+        std::cout << "error a = c"<<std::endl;
+    }
+
+
+
+
+    return 0;
+}
